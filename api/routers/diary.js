19c5589b276b1048e73179d94f6454b0b5534a83
Zaksk <zakariamar.99@gmail.com>
2025-01-31 14:04:50 +0000

    Add backend api

---
+++ api/routers/diary.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+
+const diaryController = require('../controllers/diary');
+const diaryRouter = Router();
+
+
+diaryRouter.get("/", diaryController.index);
+diaryRouter.get("/:id", diaryController.show);
+diaryRouter.get("/entry/:date", diaryController.getByDate);
+diaryRouter.patch("/:id", diaryController.update);
+diaryRouter.post("/", diaryController.create);
+diaryRouter.delete("/:id", diaryController.destroy);
+
+module.exports = diaryRouter;

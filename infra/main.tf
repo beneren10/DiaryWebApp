524bafd63586b767576df4f0c8eb33b402c51281
Ben Eren <ben@Bens-MacBook-Air.local>
2025-08-01 23:41:38 +0100

    cicd draft1

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "aks-${var.cluster_name}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "RG_CICD"
+}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "cluster_name" {
+  default = "myAKSCluster"
+}

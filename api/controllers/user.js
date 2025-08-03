4d686e300ef7b1a919fdca21c9e763a7a3bb17dd
Ben Eren <ben@Bens-MacBook-Air.local>
2025-08-03 19:21:21 +0100

    fix

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,8 @@
+const User = require('../models/User');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
 
 async function register(req, res) {
     try {

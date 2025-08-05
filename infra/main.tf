b04306fa9e21879bcab1f369ae1667f7c46ef19a
Ben Eren <ben@Bens-MacBook-Air.local>
2025-08-05 14:33:43 +0100

    secret.yaml

--- infra/main.tf
+++ infra/main.tf
@@ -2,35 +2,17 @@ data "azurerm_resource_group" "existing" {
   name = var.resource_group_name
 }
 
-# data "azurerm_kubernetes_cluster" "existing" {
-#   name                = var.cluster_name
-#   resource_group_name = data.azurerm_resource_group.existing.name
-# }
-
-resource "azurerm_kubernetes_cluster" "new" {
+data "azurerm_kubernetes_cluster" "existing" {
   name                = var.cluster_name
-  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  dns_prefix          = "myaks"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2s"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
 }
 
-
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.new.name
+  value = data.azurerm_kubernetes_cluster.existing.name
 }
 
 output "aks_cluster_fqdn" {
-  value = azurerm_kubernetes_cluster.new.fqdn
+  value = data.azurerm_kubernetes_cluster.existing.fqdn
 }
 
 output "aks_resource_group" {
